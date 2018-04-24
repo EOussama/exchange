dc5bd234611b1eb68ff7678c98398154c265b8af
EOussama <oussama_gtacj@live.fr>
2018-04-24 01:58:20 +0100

    Error redirection added

---
+++ pages/error.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="application-name" content="Exchange">
+		<meta name="author" content="Amine, Ayman">
+		<meta name="description" content="A user-friendly platform that eases exchanging goods online.">
+		<meta name="keywords" content="exchange, shopping, e-commerce">
+
+		<link rel="stylesheet" type="text/css" href="\exchange\fonts\font-awesome\css\fontawesome-all.min.css">
+		<link href="\exchange\styles\bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="\exchange\styles\main.css">
+		<link rel="icon" type="image/png" href="\exchange\images\favicon.png">
+
+		<title>Exchange</title>
+	</head>
+    
+    <body>
+    	<header>
+    		<div class="jumbotron" style="height: 100vh; padding: 200px;">
+				<h1 class="display-1 text-center"><i class="fas fa-bug"></i> Ops, file not found!</h1>
+				<hr>
+				<p class="text-center">The page you're trying to access does not exists! either that, or there were some problems while being redirected, in that case please let our support team know.</p>
+				<a href="\exchange\index.php" target="_self"><button class="btn btn-dark btn-lg btn-block">Main page</button></a>
+    		</div>
+    	</header>
+	</body>
+</html>
