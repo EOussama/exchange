359ecf068fd5d96f2b884f99df15b6eb821a4df3
EOussama <oussama_gtacj@live.fr>
2018-05-08 10:54:56 +0100

    queries updated

--- queries.sql
+++ queries.sql
@@ -22,22 +22,3 @@ CREATE TABLE `Matches`(
     CONSTRAINT pk_matchid PRIMARY KEY(`matchid`),
     CONSTRAINT fk_userid FOREIGN KEY(`userid`) REFERENCES `Users`(`userid`)
 );
-
--- Offers table
-CREATE TABLE `Offers`(
-	`offerid` INT(11) NOT NULL AUTO_INCREMENT,
-    `matchid` INT(11) NOT NULL,
-    `offer` VARCHAR(40) NOT NULL,
-    
-    CONSTRAINT pk_offerid PRIMARY KEY(`offerid`),
-    CONSTRAINT fk_matchid FOREIGN KEY(`matchid`) REFERENCES `Matches`(`matchid`)
-);
--- Needs table
-CREATE TABLE `Needs`(
-	`needid` INT(11) NOT NULL AUTO_INCREMENT,
-    `matchid` INT(11) NOT NULL,
-    `need` VARCHAR(40) NOT NULL,
-    
-    CONSTRAINT pk_needid PRIMARY KEY(`needid`),
-    CONSTRAINT fk_nmatchid FOREIGN KEY(`matchid`) REFERENCES `Matches`(`matchid`)
-);
